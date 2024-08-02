080dd0b6d2739274c0631e9c1889f44aad201622
Logan Sorese <logan@sorese.com>
2024-08-01 20:50:59 -0400

    Update bypassToken in astro.config.mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
     adapter: vercel({
         isr: {
             // A secret random string that you create.
-            bypassToken: "666hailsatanlordofdarkness666",
+            bypassToken: "666666666hailsatanlordofdarkness666666666",
             // Paths that will always be served fresh.
             exclude: [ "/index.astro", "/" ]
         }
