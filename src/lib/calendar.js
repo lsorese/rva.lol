461c1650b5a85dfb2428cd706cc9897caee6c2fd
Logan Sorese <logan@sorese.com>
2024-08-01 23:47:34 -0400

    Add console log for CALENDAR_ID variable

--- src/lib/calendar.js
+++ src/lib/calendar.js
@@ -6,6 +6,7 @@ const { RRule } = pkg;
 export async function getCalendarEvents(calendarIdEnvVar) {
   const API_KEY = import.meta.env.PUBLIC_API_KEY;
   const CALENDAR_ID = import.meta.env[calendarIdEnvVar];
+  console.log(CALENDAR_ID)
 
   const response = await axios.get(
     `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`
