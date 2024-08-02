0fdd68c3d0fac9b8263a7a3e83a05b198fa74e0f
Logan Sorese <logan@sorese.com>
2024-08-01 23:59:00 -0400

    Remove unnecessary console.log for CALENDAR_ID

--- src/lib/calendar.js
+++ src/lib/calendar.js
@@ -6,7 +6,6 @@ const { RRule } = pkg;
 export async function getCalendarEvents(calendarIdEnvVar) {
   const API_KEY = import.meta.env.PUBLIC_API_KEY;
   const CALENDAR_ID = import.meta.env[calendarIdEnvVar];
-  console.log(CALENDAR_ID, "CALENDAR ID")
   console.log(import.meta.env.PUBLIC_API_KEY, "API KEY")
   console.log(`https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`, "URL")
 
