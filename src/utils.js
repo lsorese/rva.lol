84e548aa7657f99eb69fe18129137060cef1e6a5
Logan Sorese <logan@sorese.com>
2024-08-04 11:50:06 -0400

    Refactor getCalendar to use object for calendars

--- src/utils.js
+++ src/utils.js
@@ -32,13 +32,14 @@ export function generateGoogleMapsLink(location) {
 }
 
 export function getCalendar(option) {
-  let email;
-  if (option === 'openmics') {
-    email = 'b3d8d5b621d0840f0266cfbcfaaaeb06986e135975d1bc7d73eb74d5edfeaa9f@group.calendar.google.com';
-  } else if (option === 'showcases') {
-    email = '7c54a7efcbad1512c0061bacfc4635726f1893ed9db7456b4b1b05c8b9c26561@group.calendar.google.com';
+  const calendars = {
+    openmics: 'b3d8d5b621d0840f0266cfbcfaaaeb06986e135975d1bc7d73eb74d5edfeaa9f@group.calendar.google.com',
+    showcases: '7c54a7efcbad1512c0061bacfc4635726f1893ed9db7456b4b1b05c8b9c26561@group.calendar.google.com',
+  };
+
+  if (option in calendars) {
+    return calendars[option];
   } else {
     throw new Error('Invalid option');
   }
-  return email;
 }
