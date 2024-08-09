4a9db37d7923fac51913c36372994ec5c158c94e
Logan Sorese <logan@sorese.com>
2024-08-08 21:04:25 -0400

    Update site URL to 'https://rva.lol' in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
-import sitemap from "@astrojs/sitemap";
-
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rva.lol',
   output: "server",
   integrations: [mdx(), react(), sitemap()],
   adapter: vercel({
