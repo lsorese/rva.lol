15a64dadf87d19eba671c813912299ae7598ca6e
Logan Sorese <logan@sorese.com>
2024-08-08 21:00:55 -0400

    Add sitemap integration and meta tags for SEO enhancement

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,19 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
-    output: "server",
-    integrations: [mdx(), react()],
-    adapter: vercel({
-        isr: {
-            // A secret random string that you create.
-            bypassToken: "hailsatanlordofdarkness66666666666666666",
-            // Uncache everything every 4 minutes
-            maxAge: 240,
-            exclude: ["/", "/shows" ]
-        }
-    })
-})
+  output: "server",
+  integrations: [mdx(), react(), sitemap()],
+  adapter: vercel({
+    isr: {
+      // A secret random string that you create.
+      bypassToken: "hailsatanlordofdarkness66666666666666666",
+      // Uncache everything every 4 minutes
+      maxAge: 240,
+      exclude: ["/", "/shows"]
+    }
+  })
+});
