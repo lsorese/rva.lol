841f64981c34261114b9b378c05ac07caaf5a7d4
Logan Sorese <logan@sorese.com>
2024-08-08 21:11:38 -0400

    Update Vercel integration settings for caching headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,38 @@ import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-// https://astro.build/config
+
 export default defineConfig({
-  site: 'https://rva.lol',
-  output: "server",
-  integrations: [mdx(), react(), sitemap()],
-  adapter: vercel({
-    isr: {
-      // A secret random string that you create.
-      bypassToken: "hailsatanlordofdarkness66666666666666666",
-      // Uncache everything every 4 minutes
-      maxAge: 240,
-      exclude: ["/", "/shows"]
-    }
-  })
-});
+    site: 'https://rva.lol',
+    output: "server",
+    integrations: [mdx(), react(), sitemap()],
+    adapter: vercel({
+      isr: {
+        // A secret random string that you create.
+        bypassToken: "hailsatanlordofdarkness66666666666666666",
+        // Uncache everything else every 4 minutes
+        maxAge: 240,
+        exclude: [] // Ensure all pages are cached
+      },
+      headers: [
+        {
+          source: "/", // Apply to homepage
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "s-maxage=3600, stale-while-revalidate"
+            }
+          ]
+        },
+        {
+          source: "/shows", // Apply to /shows page
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "s-maxage=3600, stale-while-revalidate"
+            }
+          ]
+        }
+      ]
+    })
+  });
